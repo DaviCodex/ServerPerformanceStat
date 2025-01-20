27375c0de0a2d7f639a7c824fd8daf2ad5c70cc6
DaviCodex <davidfvm2002@gmail.com>
2025-01-19 22:36:12 -0500

    Getitng the CPU usage

--- server-status.sh
+++ server-status.sh
@@ -1,3 +1,8 @@
 #! /bin/bash 
-top -n 1 | grep %Cpu > texto.txt
+
+#Creating a loop to simulate a task
+while true; do :; done &
+# Getting the CPU usage
+top -bn 1 | grep %Cpu | awk '{print 100-$4}'
+
 
