21a4abb767ee50f8a8d368d53144b7f9463b5cc5
DaviCodex <davidfvm2002@gmail.com>
2025-01-20 22:31:24 -0500

    Fix percentage and prints

--- server-status.sh
+++ server-status.sh
@@ -4,5 +4,14 @@
 while true; do :; done &
 # Getting the CPU usage
 top -bn 1 | grep %Cpu | awk '{print 100-$4}'
-
+# Free memory in MB 
+freeMem=free -m | awk 'NR==2 {print $3}'
+# Memory in use in MB 
+inUse=free -m | awk 'NR==2 {print $2}'
+#on percentage
+freePerc= $freeMem / $inUse * 100
+inUsePerc= (($free / $inUse)*-1 * 100)*-1
+echo "The free memory is: $freeMem MB and in percentage: $freePerc %"
+echo "The memory in use is: $inUse MB and in percentage: $inUsePerc %"
+#on percentage
 
