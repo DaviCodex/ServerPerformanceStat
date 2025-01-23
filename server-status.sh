f143e48ec393e714b9a81470d6ef137bd94b2bd2
DaviCodex <davidfvm2002@gmail.com>
2025-01-22 22:41:47 -0500

    Memory in use and free

--- server-status.sh
+++ server-status.sh
@@ -1,17 +1,17 @@
 #! /bin/bash 
-
 #Creating a loop to simulate a task
 while true; do :; done &
 # Getting the CPU usage
-top -bn 1 | grep %Cpu | awk '{print 100-$4}'
+CPU_usage=$(top -bn 1 | grep %Cpu | awk '{print 100-$4}')
+echo "The CPU % in use is: $CPU_usage %"
 # Free memory in MB 
-freeMem=free -m | awk 'NR==2 {print $3}'
+freeMem=$(free -m | awk 'NR==2 {print $3}')
 # Memory in use in MB 
-inUse=free -m | awk 'NR==2 {print $2}'
-#on percentage
-freePerc= $freeMem / $inUse * 100
-inUsePerc= (($free / $inUse)*-1 * 100)*-1
-echo "The free memory is: $freeMem MB and in percentage: $freePerc %"
-echo "The memory in use is: $inUse MB and in percentage: $inUsePerc %"
-#on percentage
+inUse=$(free -m | awk 'NR==2 {print $2}')
+#Free memory percentage
+per_free=$(echo "scale=2; ($freeMem / $inUse)*100" | bc -l)
+echo "The memory in use is: $freeMem MB and in percentage: $per_free %"
+per_use=$( echo "scale=2; (1-($freeMem / $inUse))*100" | bc -l)
+echo "The memory in use is: $inUse MB and in percentage: $per_use %"
+
 
