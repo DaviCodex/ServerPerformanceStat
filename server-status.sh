9a57120b585168590fd44c652c234a5513089400
DaviCodex <davidfvm2002@gmail.com>
2025-01-26 08:23:39 -0500

    Disk in use vs disk free complete

--- server-status.sh
+++ server-status.sh
@@ -1,9 +1,21 @@
 #! /bin/bash 
-#Creating a loop to simulate a task
+####################################
+#Creating a loop to simulate a task#
+####################################
+
 while true; do :; done &
-# Getting the CPU usage
+
+###################################
+###### Getting the CPU usage#######
+###################################
+
 CPU_usage=$(top -bn 1 | grep %Cpu | awk '{print 100-$4}')
 echo "The CPU % in use is: $CPU_usage %"
+
+###################################
+### Free memory vs memory in use###
+###################################
+
 # Free memory in MB 
 freeMem=$(free -m | awk 'NR==2 {print $3}')
 # Memory in use in MB 
@@ -14,4 +26,11 @@ echo "The memory in use is: $freeMem MB and in percentage: $per_free %"
 per_use=$( echo "scale=2; (1-($freeMem / $inUse))*100" | bc -l)
 echo "The memory in use is: $inUse MB and in percentage: $per_use %"
 
+##################################
+#### Disk in use vs disk free ####
+##################################
 
+disk_in_use=$(df -h / | awk 'NR==2 {print $4}')
+disk_free=$(df -h / | awk 'NR==2 {print $3}')
+
+echo "The disk in use is: $disk_in_use and the disk free is: $disk_free"
